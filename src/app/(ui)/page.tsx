50b774c1a84df77f4663c466d1ceb17bc2253c4b
Frangiskos <frangiskos@gmail.com>
2023-07-17 01:51:29 +0300

    homepage

--- src/app/(ui)/page.tsx
+++ src/app/(ui)/page.tsx
@@ -1,10 +1,45 @@
+import Image from 'next/image';
+
 export default function HomePage() {
   return (
-    <div className="bg-green-800 text-white flex flex-col items-center justify-center text-center space-y-6 py-20">
-      <h1 className="text-4xl font-bold">Welcome to ecoblocks</h1>
-      <p className="font-light text-2xl">Recycle your devices responsibly and earn rewards</p>
-
-      <div className="h-64 w-64 bg-green-300 rounded-full"></div>
-    </div>
+    <section className="bg-white dark:bg-gray-900 p-10">
+      <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
+        <div className="mr-auto place-self-center lg:col-span-7">
+          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+            Welcome to ecoblocks
+          </h1>
+          <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
+            Recycle your devices responsibly and earn rewards
+          </p>
+          <a
+            href="./seller"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+          >
+            Sellers
+            <svg
+              className="w-5 h-5 ml-2 -mr-1"
+              fill="currentColor"
+              viewBox="0 0 20 20"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                fillRule="evenodd"
+                d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+                clipRule="evenodd"
+              ></path>
+            </svg>
+          </a>
+          <a
+            href="./consumers"
+            className="inline-flex items-center justify-center px-5 py-3 text-base font-medium text-center text-gray-900 border border-gray-300 rounded-lg hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
+          >
+            Learn More
+          </a>
+        </div>
+        <div className="hidden lg:mt-0 lg:col-span-5 lg:flex">
+          <Image src={'/images/home/hero.jpg'} alt="save the planet" width={1000} height={1000} />
+        </div>
+      </div>
+    </section>
   );
 }
